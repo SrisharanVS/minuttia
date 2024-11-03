eb6324d8c7ae53f3a3b5e71733b2faa4d0f33226
“SrisharanVS” <srisharan.psgtech@gmail.com>
2024-11-03 13:26:15 +0530

    first commit

---
+++ minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil.jpg 792w, https_/minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-300x300.jpg 300w, https_/minuttia.com/wp-cont/Jitesh-Patil-96x96.e6.d
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-5 [XR&CO'2014] -->
+
+<!-- Mirrored from minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil.jpg 792w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-300x300.jpg 300w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-150x150.jpg 150w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-768x768.jpg 768w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-700x700.jpg 700w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-24x24.jpg 24w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-48x48.jpg 48w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-96x96.jpg 96w by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 03 Nov 2024 04:07:16 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=Jitesh-Patil-96x96.e6.d"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="Jitesh-Patil-96x96.e6.d"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-5 [XR&CO'2014] -->
+
+<!-- Mirrored from minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil.jpg 792w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-300x300.jpg 300w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-150x150.jpg 150w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-768x768.jpg 768w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-700x700.jpg 700w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-24x24.jpg 24w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-48x48.jpg 48w, https://minuttia.com/wp-content/uploads/2024/02/Jitesh-Patil-96x96.jpg 96w by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 03 Nov 2024 04:07:16 GMT -->
+</HTML>
